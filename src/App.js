f4923a90f7046e913e9d91897eb85f5a94bc38bb
NwizuEmmauel <nwizuemmanuel200@gmail.com>
2022-07-12 16:49:41 +0100

    added img

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import pic from "./assets/pic1.jpg";
 
 const App = () => {
-  return <div>Hello</div>;
+  return (
+    <>
+      <Container fluid className="mt-2">
+        <img src={pic} alt="pic" style={{ height: 500, width: 300 }} />
+        <Button variant="primary">primary</Button>
+      </Container>
+    </>
+  );
 };
 
 export default App;
